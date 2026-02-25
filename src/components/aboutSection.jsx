33a553d30433606ad003858fab5c9a46bb7e364c
Sanjana Kumari <86369138+Sanjnak@users.noreply.github.com>
2026-02-25 17:46:58 +0530

    Update aboutSection.jsx

--- src/components/aboutSection.jsx
+++ src/components/aboutSection.jsx
@@ -34,7 +34,7 @@ const AboutSection = () => {
                 Get In Touch
               </a>
               <a
-                href=""
+                href="https://drive.google.com/file/d/1DluMt5NmwMuX0WDeSZpTMC30UPMi7JY5/view?usp=sharing"
                 className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300"
               >
                 {" "}
